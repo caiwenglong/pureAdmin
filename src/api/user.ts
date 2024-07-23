292c20976eeb157fc87eead0700eb76405bb6b6d
cwl <836472117@qq.com>
2024-07-23 11:24:51 +0800

    init

---
+++ src/api/user.ts
@@ -0,0 +1,45 @@
+import { http } from "@/utils/http";
+
+export type UserResult = {
+  success: boolean;
+  code: Number;
+  data: {
+    /** 头像 */
+    avatar: string;
+    /** 用户名 */
+    username: string;
+    /** 昵称 */
+    nickname: string;
+    /** 当前登录用户的角色 */
+    roles: Array<string>;
+    /** `token` */
+    token: string;
+    /** 用于调用刷新`token`的接口时所需的`token` */
+    refreshToken: string;
+    /** `token`的过期时间（格式'xxxx/xx/xx xx:xx:xx'） */
+    expires: Date;
+  };
+};
+
+export type RefreshTokenResult = {
+  success: boolean;
+  code: Number;
+  data: {
+    /** `token` */
+    token: string;
+    /** 用于调用刷新`token`的接口时所需的`token` */
+    refreshToken: string;
+    /** `token`的过期时间（格式'xxxx/xx/xx xx:xx:xx'） */
+    expires: Date;
+  };
+};
+
+/** 登录 */
+export const getLogin = (data?: object) => {
+  return http.request<UserResult>("post", "/login", { data });
+};
+
+/** 刷新`token` */
+export const refreshTokenApi = (data?: object) => {
+  return http.request<RefreshTokenResult>("post", "/refresh-token", { data });
+};
